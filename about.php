2d401f0e4ccdf62bf33d21d5ceaf048149896dbc
Kasper Isager <kasperisager@gmail.com>
2013-03-09 14:30:22 +0100

    Update description

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
 $ThemeInfo['Ninja'] = array(
 
     'Name' => 'Ninja',
-    'Description' => 'Ninja is a super sneaky Node.js and Grunt powered scaffold for building superb Vanilla themes. Ninja allows you to easily use your choice of preprocessors and languages in your themes and enables you to write kick-ass build processes using the many plugins available for Grunt.<p></p>Ninja will also stealthily do browser sniffing and feature detection using Conditionizr and Modernizr and will also reload your assets whenever they change using LiveReload - like a true shinobi!',
+    'Description' => 'Ninja is a super sneaky Node.js, Grunt and Bower powered scaffold for building superb Vanilla themes. Ninja allows you to easily use your choice of preprocessors and languages in your themes and enables you to write kick-ass build processes using the many plugins available for Grunt.<p></p>Ninja will also stealthily do browser sniffing and feature detection using Conditionizr and Modernizr and will also reload your assets whenever they change using LiveReload - like a true shinobi!',
     'Version' => '1.0.0',
     'Author' => 'Kasper K. Isager',
     'AuthorEmail' => 'kasperisager@gmail.com',
