ca034d3b3e5871fd14ea58885d211477dd1015a0
Kasper Isager <kasperisager@gmail.com>
2014-03-05 10:17:18 -0500

    Bump version to 2.0.3

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['ninja'] = array(
     'Name'        => 'Ninja',
     'Description' => 'Ninja is a super sneaky Node.js, Gulp and Bower powered scaffold for building superb Vanilla themes. Ninja allows you to easily use your choice of preprocessors and languages in your themes and enables you to write kick-ass build processes using the many plugins available for Gulp.<p></p>Ninja will also stealthily do browser sniffing and feature detection using Conditionizr and Modernizr and will also reload your assets whenever they change using LiveReload - like a true shinobi!',
-    'Version'     => '2.0.2',
+    'Version'     => '2.0.3',
     'Author'      => 'Kasper K. Isager',
     'AuthorEmail' => 'kasperisager@gmail.com',
     'AuthorUrl'   => 'https://github.com/kasperisager',
